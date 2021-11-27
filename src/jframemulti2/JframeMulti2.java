974b9a4e9bdc6f44466fe744addfcff752ff3d65
Rafiirham <88201166+Rafiirham@users.noreply.github.com>
2021-11-27 22:14:09 +0700

    Add files via upload

---
+++ src/jframemulti2/JframeMulti2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jframemulti2;
+
+/**
+ *
+ * @author ACER
+ */
+public class JframeMulti2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Menuseleksi NT = new Menuseleksi();
+        NT.setVisible(true);
+    }
+    
+}
